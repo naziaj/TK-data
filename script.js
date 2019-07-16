b55ca5a6bce56f294acc01320ccade9d7d4754a5
naziaj <49521887+naziaj@users.noreply.github.com>
2019-07-16 12:24:59 +0500

    WIP

---
+++ script.js
@@ -0,0 +1,69 @@
+
+let techKaro = {
+    courseName:"Tech Karo",
+    students:[
+        {
+            name:
+            fatherName:
+            cnic:
+            fatherOccupation:
+            profession:
+            hobbies:
+            contact:
+            email:
+            qualification:
+            gender:
+            rollNo:
+            group:
+            maritalStatus:
+            fatherIncome:
+            noOfSiblings:
+            heardFrom:
+            extraSkills:
+            isEmployed:
+            employementInfo:[
+                designation:
+                distance
+            ]
+    }
+]
+    sponsors:[]
+    batchNo: 2019,
+    faculty:[
+        {
+            name:
+            designation:
+            qualification:
+            abbout:
+            contact:
+            email:
+
+        }
+    ]
+    embassadors:[
+        {
+            name:
+            qualification:
+            profilePic:
+        }
+    ]
+    duration:
+    fees:
+    expenses:
+    founder:
+    classes:
+    location:
+    mentors:
+    lifeSkills:
+
+
+
+
+
+
+
+
+
+}
+
+
